17a5caf542384ca123fee0fde407b15c3ab7e96f
SosinaSisay <sosinasisay29@gmail.com>
2022-04-08 12:30:00 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
+ *main - entry point
  *
- *  *main - entry point
- *
- *   *
- *
- *    *Return: 0 after printing the function
- *
- *     */
-
+ *Return: 0 after printing the function
+ */
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+        return (0);
 }
