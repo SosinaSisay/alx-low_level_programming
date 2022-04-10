1d8f6e6ec48a98b45be20e906a9c0d51c9878f34
SosinaSisay <sosinasisay29@gmail.com>
2022-04-10 15:27:33 +0300

    my commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*Return: Always 0 (success/correct)
+*
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
