ea524b84858089000b5e4cd513d240f2f957e6b0
SosinaSisay <101405985+SosinaSisay@users.noreply.github.com>
2022-04-26 17:14:48 +0300

    My commit
    My commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
-#include <main.h>
+#include "main.h"
 /**
  * _strchr - Locates a character in a string
  * @s: String
  * @c: Character to search
  * Return: Pointer to the first occurrence of the character c in the string s
- */
+ **/
 char *_strchr(char *s, char c)
 {
 while (*s != '\0') /*Declaring WHILE*/
@@ -17,8 +17,8 @@ while (*s != '\0') /*Declaring WHILE*/
 }
 if (*s == c)
 {
-		return (s);
+   return (s);
 }
-return (0);/* values null*/
+   return (0);/* values null*/
 }
 
